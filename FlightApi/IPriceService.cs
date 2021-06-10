3bd2add0198d6965de3606ced0ac5acc83b26322
Andy Sterland <andster@microsoft.com>
2021-06-10 09:01:37 -0700

    Fix typo

--- FlightApi/IPriceService.cs
+++ FlightApi/IPriceService.cs
@@ -9,8 +9,6 @@ namespace FlightApi
     public interface IPriceService
     {
         public Task<List<Price>> GetPrice(string DepartureAirportCode, string ArrivalAirportCode, DateTime Date);
-        public Task<List<Price>> GetPrice(IAirportService airportService, IFlightDestinationService flightService, string DepartureAirportCode, DateTime Date)
-
-
+        public Task<List<Price>> GetPrice(IAirportService airportService, IFlightDestinationService flightService, string DepartureAirportCode, DateTime Date);
     }
 }
